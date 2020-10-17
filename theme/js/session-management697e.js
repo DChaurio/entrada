95b7d205f57a3ee13e93d2ce793304b1f76b94cf
DChaurio <70455014+DChaurio@users.noreply.github.com>
2020-10-17 19:10:05 -0400

    Add files via upload

---
+++ theme/js/session-management697e.js
@@ -0,0 +1,46 @@
+var userKey = "_pbid";
+var expireDays = 730; // Dos años
+
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        var expires = "";
+    }
+    document.cookie = name + "=" + value + expires + ";domain=.patria.org.ve; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+function generateBrowserID() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4() + (new Date()).getTime();
+}
+
+$(document).ready(function () {
+    if (readCookie(userKey) === null) {
+        createCookie(userKey, generateBrowserID(), expireDays);
+    }
+});
